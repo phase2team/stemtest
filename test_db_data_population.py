f66473928addefa23a9526901414ea752b8e46c6
Ken Kranz <kenkranz@Toshas-Air-2.local>
2020-07-13 21:47:55 -0400

    Create test functions to add data

--- test_db_data_population.py
+++ test_db_data_population.py
@@ -25,56 +25,67 @@ print("\n=========", __file__, "=========\n")
 # print(students)
 
 #  Add student info to database
-chattStateANumber = "A12"
-if len(Student.query.filter_by(chattStateANumber=chattStateANumber).all()) == 0:
-    student1 = Student(
-        chattStateANumber=chattStateANumber,
-        firstName="Max",
-        lastName="Tester",
-        email="testy@students.hcde.org",
-        house="Staupers",
-        yearOfGraduation=2021,
-    )
-    print(student1)
-    db.session.add(student1)
-    db.session.commit()
-else:
-    print("Student with chattStateANumber =", chattStateANumber, "already exists")
+def test_addStudent():
+    chattStateANumber = "A12"
+    if len(Student.query.filter_by(chattStateANumber=chattStateANumber).all()) == 0:
+        student1 = Student(
+            chattStateANumber=chattStateANumber,
+            firstName="Max",
+            lastName="Tester",
+            email="testy@students.hcde.org",
+            house="Staupers",
+            yearOfGraduation=2021,
+        )
+        print(student1)
+        db.session.add(student1)
+        db.session.commit()
+    else:
+        print("Student with chattStateANumber =", chattStateANumber, "already exists")
+
 
 # # print(Student.query.all())
 
 # Add class schedule information to database
-# classSchedule1 = ClassSchedule(
-#     className="English",
-#     startTime=datetime(2020, 8, 15, 9, 30, 0),
-#     endTime=datetime(2020, 8, 15, 10, 30, 0),
-#     classDays="MW",
-#     student_id=1,
-# )
-# print(classSchedule1)
-# db.session.add(classSchedule1)
-# db.session.commit()
+def test_addClassSchedule():
+    classSchedule1 = ClassSchedule(
+        className="English",
+        startTime=datetime(2020, 8, 15, 9, 30, 0),
+        endTime=datetime(2020, 8, 15, 10, 30, 0),
+        classDays="MW",
+        student_id=1,
+    )
+    print(classSchedule1)
+    db.session.add(classSchedule1)
+    db.session.commit()
+
 
 # Add staff to database
-# staff1 = FacultyAndStaff(
-#     firstName="Betsy",
-#     lastName="Ross",
-#     position="Art Teacher",
-#     email="test@demo.com",
-#     phoneNumber="423-555-1212",
-#     chattStateANumber="A2",
-# )
-# print(staff1)
-# db.session.add(staff1)
-# db.session.commit()
+def test_addStaff():
+    staff1 = FacultyAndStaff(
+        firstName="Betsy",
+        lastName="Ross",
+        position="Art Teacher",
+        email="test@demo.com",
+        phoneNumber="423-555-1212",
+        chattStateANumber="A2",
+    )
+    print(staff1)
+    db.session.add(staff1)
+    db.session.commit()
 
-# dailyAttendanceLog1 = DailyAttendanceLog(
-#     absenceDate=datetime(2020, 9, 20),
-#     attendanceCode="E",
-#     comment="Sick",
-#     staffID=2,
-#     student_id=1,
-# )
-# print(dailyAttendanceLog1)
-# db.session.add(dailyAttendanceLog1)
-# db.session.commit()
+
+# Add daily attendance log to database
+def test_addDailyAttendanceLog():
+    dailyAttendanceLog1 = DailyAttendanceLog(
+        absenceDate=datetime(2020, 9, 15),
+        attendanceCode="E",
+        comment="Sick",
+        staffID=2,
+        student_id=1,
+    )
+    print(dailyAttendanceLog1)
+    db.session.add(dailyAttendanceLog1)
+    db.session.commit()
+
+
+test_addDailyAttendanceLog()
