fa4bfc9063db72fad75b0ccea55b77d49b51e747
Ken Kranz <kenkranz@Toshas-Air-2.local>
2020-08-16 09:45:01 -0400

    Added Google Cloud Debugger

--- main.py
+++ main.py
@@ -2,5 +2,14 @@ from P2MT_App import create_app
 
 app = create_app()
 
+# Google Debugger
+try:
+  import googleclouddebugger
+  googleclouddebugger.enable(
+    breakpoint_enable_canary=True
+  )
+except ImportError:
+  pass
+
 if __name__ == "__main__":
     app.run(debug=True)
