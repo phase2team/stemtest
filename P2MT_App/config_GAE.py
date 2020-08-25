f52ab5434fa37e4c96a38395169b4b81554aef68
Ken Kranz <kenkranz@Toshas-Air-2.local>
2020-08-25 16:18:49 -0400

    Added config param to prevent CSRF expired error

--- P2MT_App/config_GAE.py
+++ P2MT_App/config_GAE.py
@@ -16,3 +16,4 @@ class Config:
     USE_GOOGLE_LOGIN_AND_API = "True"
     GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
     GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
+    WTF_CSRF_TIME_LIMIT = None
